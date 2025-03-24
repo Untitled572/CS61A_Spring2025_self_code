0aacc6baf71488501551912aba09dad36caea764
Untitled572 <rongchangjin@gmail.com>
2025-03-24 01:15:31 +0000

    Mon, Mar 24, 2025, 9:15 AM +08:00

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -109,9 +109,10 @@ def make_repeater(f, n):
     >>> make_repeater(square, 3)(5) # square(square(square(5)))
     390625
     """
-    def func(x):
-        return f(x)
-    for i in range(1, n+1):
-        func(f(x))
-
-    return func
+    def mul_func(x):
+        def func(x):
+            return f(x)
+        for i in range(1, n+1):
+            func(f(x))
+    return 
+    return mul_func
