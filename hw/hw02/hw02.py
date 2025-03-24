87ef85da6621490fea485f0efa2de380717ff353
Untitled572 <rongchangjin@gmail.com>
2025-03-24 00:17:46 +0000

    Mon, Mar 24, 2025, 8:17 AM +08:00

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -31,7 +31,9 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    
+    "*** YOUR CODE HERE ***"
+
+
 def accumulate(fuse, start, n, term):
     """Return the result of fusing together the first n terms in a sequence
     and start.  The terms to be fused are term(1), term(2), ..., term(n).
