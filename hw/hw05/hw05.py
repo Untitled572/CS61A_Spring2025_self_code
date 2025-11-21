258947fd928b53853785486e98fb244612d0ca75
Untitled572 <rongchangjin@gmail.com>
2025-11-21 09:08:31 +0800

    a lot

--- hw/hw05/hw05.py
+++ hw/hw05/hw05.py
@@ -21,7 +21,15 @@ def hailstone(n):
     >>> next(hail_gen)
     1
     """
-    
+    while True:
+        if n == 1:
+            yield n
+        elif n % 2 == 1:
+            yield n
+            n = n * 3 + 1
+        elif n % 2 == 0:
+            yield n
+            n = n // 2
 
 
 def merge(a, b):
@@ -42,11 +50,15 @@ def merge(a, b):
     a_val, b_val = next(a), next(b)
     while True:
         if a_val == b_val:
-            "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val, b_val = next(a), next(b)
         elif a_val < b_val:
-            "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val = next(a)
         else:
-            "*** YOUR CODE HERE ***"
+            yield b_val
+            b_val = next(b)
+
 
 
 def stair_ways(n):
@@ -62,7 +74,6 @@ def stair_ways(n):
     >>> list(s_w) # Ensure you're not yielding extra
     []
     """
-    "*** YOUR CODE HERE ***"
 
 
 def yield_paths(t, value):
@@ -101,10 +112,10 @@ def yield_paths(t, value):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == value:
-        yield ____
+        yield ...
     for b in branches(t):
         for ____ in ____:
-            yield ____
+            yield ...
 
 
 
