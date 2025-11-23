eecbbee8f14ced89066454759d1df81438d9a087
Untitled572 <rongchangjin@gmail.com>
2025-11-23 17:51:13 +0800

    hw07 finished

---
+++ hw/hw07/hw07.scm
@@ -0,0 +1,27 @@
+(define (square n) (* n n))
+
+(define (pow base exp)
+    (cond ((= exp 2) (square base))
+        ((= exp 1) base)
+        (else (if (odd? exp)
+                (* base (pow base (- exp 1)))
+                (square (pow base (/ exp 2)))
+            )
+        )
+    )
+)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let ((y (repeatedly-cube (- n 1) x)) )
+        (* y y y)
+        )
+    )
+)
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
